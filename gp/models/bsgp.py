0731599abc8703733e62ce1fecbab2e159901baf
shixinxing <47957281+shixinxing@users.noreply.github.com>
2024-11-26 18:26:21 +0000

    update to torch 2.x

--- gp/models/bsgp.py
+++ gp/models/bsgp.py
@@ -8,37 +8,34 @@ from ..conditionals import conditional, conditional2
 
 
 class Strauss(nn.Module):
-    
     def __init__(self, gamma=0.5, R=0.5):
         super(Strauss, self).__init__()
         self.gamma = gamma
         self.R = R
 
     def _euclid_dist(self, X):
-        Xs = torch.sum(torch.square(X), dims=-1, keepdims=True)
+        Xs = torch.sum(torch.square(X), dim=-1, keepdim=True)
         dist = -2 * X @ X.T
         dist += Xs + Xs.T
 
-        return torch.sqrt(torch.maximum(dist, 1e-40))
-    
+        return torch.sqrt(torch.maximum(dist, torch.tensor(1e-40, dtype=dist.dtype)))
+
     def _get_Sr(self, X):
         """
         Get the # elements in distance matrix dist that are < R
         """
         dist = self._euclid_dist(X)
         val = torch.where(dist <= self.R)
-        Sr = val.shape[0] # number of points satisfying the constraint above
+        Sr = val[0].shape[0]  # number of points satisfying the constraint above
         dim = dist.shape[0]
-        Sr = (Sr - dim)/2  # discounting diagonal and double counts
+        Sr = (Sr - dim) / 2  # discounting diagonal and double counts
         return Sr
 
     def log_prob(self, X):
         return self._get_Sr(X) * np.log(self.gamma)
 
 
-
 class BSGP(nn.Module):
- 
     def __init__(self, X, Y, kernel, likelihood, prior_type, inputs, outputs,
                  n_data=None, n_inducing=None, inducing_points_init=None, full_cov=False, prior_lengthscale=2.,
                  prior_variance=0.05, prior_lik_var=0.05):
@@ -52,7 +49,7 @@ class BSGP(nn.Module):
         self.prior_lengthscale = prior_lengthscale
         self.prior_variance = prior_variance
         self.prior_lik_var = prior_lik_var
-        
+
         if n_data is None:
             self.N = X.shape[0]
         else:
@@ -64,51 +61,46 @@ class BSGP(nn.Module):
             if torch.is_tensor(X):
                 X = X.cpu().numpy()
 
-            inducing_points_init = torch.tensor(
-                kmeans2(X, self.M, minit='points')[0], dtype=torch.float64)
+            inducing_points_init = torch.tensor(kmeans2(X, self.M, minit='points')[0], dtype=torch.get_default_dtype())
 
         if prior_type == "strauss":
             self.pZ = Strauss(R=0.5)
-        
-        self.Z = nn.parameter.Parameter(
-            inducing_points_init.double(),
-            requires_grad=True)
 
-        self.U = nn.parameter.Parameter(
-            torch.zeros((self.M, self.outputs), dtype=torch.float64),
-            requires_grad=True)
-
+        self.Z = nn.Parameter(inducing_points_init, requires_grad=True)
+        self.U = nn.Parameter(torch.zeros((self.M, self.outputs), dtype=torch.get_default_dtype()), requires_grad=True)
         self.Lm = None
 
     def log_prior_Z(self):
         if self.prior_type == "uniform":
             return 0.
-        
+
         if self.prior_type == "normal":
             return -torch.sum(torch.square(self.Z)) / 2.0
-        
+
         if self.prior_type == "strauss":
             return self.pZ.log_prob(self.Z)
 
-        #if self.Lm is not None: # determinantal;
+        # if self.Lm is not None: # determinantal;
         if self.prior_type == "determinantal":
-            self.Lm = torch.cholesky(self.kern.K(self.Z) + torch.eye(self.M, dtype=torch.float64, device=self.Z.device) * 1e-7)
+            self.Lm = torch.linalg.cholesky(
+                self.kern.K(self.Z) + torch.eye(self.M, dtype=torch.float64, device=self.Z.device) * 1e-7
+            )
             log_prob = torch.sum(torch.log(torch.square(torch.diagonal(self.Lm))))
             return log_prob
-        
+
         else:
             raise Exception("Invalid prior type")
 
     def conditional(self, X):
-        mean, var, self.Lm = conditional(X, self.Z, self.kern, self.U,
-                                         whiten=True, full_cov=self.full_cov,
-                                         return_Lm=True)
+        mean, var, self.Lm = conditional(
+            X, self.Z, self.kern, self.U, whiten=True, full_cov=self.full_cov, return_Lm=True
+        )
         return mean, var
 
     def predict(self, X):
         f_mean, f_var = self.conditional(X)
         y_mean, y_var = self.likelihood.predict_mean_and_var(f_mean, f_var)
-        
+
         return y_mean, y_var
 
     def log_prior_hyper(self):
@@ -146,9 +138,7 @@ class BSGP(nn.Module):
         return log_prob
 
 
-
 class BSGPTitsias(nn.Module):
- 
     def __init__(self, X, Y, kernel, likelihood, prior_type, inputs, outputs,
                  n_data=None, n_inducing=None, inducing_points_init=None, full_cov=False, prior_lengthscale=2.,
                  prior_variance=0.05, prior_lik_var=0.05):
@@ -162,7 +152,7 @@ class BSGPTitsias(nn.Module):
         self.prior_lengthscale = prior_lengthscale
         self.prior_variance = prior_variance
         self.prior_lik_var = prior_lik_var
-        
+
         if n_data is None:
             self.N = X.shape[0]
         else:
@@ -179,12 +169,12 @@ class BSGPTitsias(nn.Module):
 
         if prior_type == "strauss":
             self.pZ = Strauss(R=0.5)
-        
-        self.Z = nn.parameter.Parameter(
+
+        self.Z = nn.Parameter(
             inducing_points_init.double(),
             requires_grad=True)
 
-        self.U = nn.parameter.Parameter(
+        self.U = nn.Parameter(
             torch.zeros((self.M, self.outputs), dtype=torch.float64),
             requires_grad=True)
 
@@ -193,19 +183,20 @@ class BSGPTitsias(nn.Module):
     def log_prior_Z(self):
         if self.prior_type == "uniform":
             return 0.
-        
+
         if self.prior_type == "normal":
             return -torch.sum(torch.square(self.Z)) / 2.0
-        
+
         if self.prior_type == "strauss":
             return self.pZ.log_prob(self.Z)
 
-        #if self.Lm is not None: # determinantal;
+        # if self.Lm is not None: # determinantal;
         if self.prior_type == "determinantal":
-            self.Lm = torch.cholesky(self.kern.K(self.Z) + torch.eye(self.M, dtype=torch.float64, device=self.Z.device) * 1e-7)
+            self.Lm = torch.linalg.cholesky(
+                self.kern.K(self.Z) + torch.eye(self.M, dtype=torch.float64, device=self.Z.device) * 1e-7)
             log_prob = torch.sum(torch.log(torch.square(torch.diagonal(self.Lm))))
             return log_prob
-        
+
         else:
             raise Exception("Invalid prior type")
 
@@ -214,17 +205,17 @@ class BSGPTitsias(nn.Module):
                                          whiten=True, full_cov=self.full_cov,
                                          return_Lm=True)
         return mean, var
-    
+
     def conditional2(self, X):
         mean, var, trace = conditional2(X, self.Z, self.kern, self.U,
-                                         whiten=True, full_cov=self.full_cov,
-                                         return_trace=True)
+                                        whiten=True, full_cov=self.full_cov,
+                                        return_trace=True)
         return mean, var, trace
 
     def predict(self, X):
         f_mean, f_var = self.conditional(X)
         y_mean, y_var = self.likelihood.predict_mean_and_var(f_mean, f_var)
-        
+
         return y_mean, y_var
 
     def log_prior_hyper(self):
@@ -256,7 +247,7 @@ class BSGPTitsias(nn.Module):
         log_likelihood, trace = self.log_likelihood(X, Y)
 
         batch_size = X.shape[0]
-        
+
         log_prob = (self.N / batch_size) * log_likelihood + trace
 
         return log_prob
